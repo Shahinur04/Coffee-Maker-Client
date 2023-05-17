e0c870a0e1457c5df0f729fd723a7d2c63e6aeb1
HmSani6699 <shahinur04042002@gmail.com>
2023-05-17 14:13:29 +0600

    first commit

---
+++ src/Components/AddCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h1 className='text-center text-slate-700'>AddCoffee</h1>
+        </div>
+    );
+};
+
+export default AddCoffee;
